d814c1fcffc0c4a995ef73de8bfba282bc8cf8db
ksamrat224 <ksamrat224@gmail.com>
2025-06-26 19:05:43 +0545

    a normal form tag created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,22 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Testing 123</h1>
+    <div style="border:3px solid black;">
+        <h2>Register</h2>
+        <form action="">
+            <input type="text" placeholder="name">
+            <input type="email" placeholder="email">
+            <input type="password" placeholder="password">
+            <button>Register</button>
+
+        </form>
+    </div>
 </body>
+
 </html>
