ca347e8215bc6bf271313dbd5ead0605ecbca632
ksamrat224 <ksamrat224@gmail.com>
2025-06-28 19:34:30 +0545

    import error fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
