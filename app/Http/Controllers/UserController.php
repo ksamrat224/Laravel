a8bbf0e754eb4577f4b6be553dd7234a144f829b
ksamrat224 <ksamrat224@gmail.com>
2025-06-28 19:54:56 +0545

    login implemented successfully

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,23 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
-{   public function logout (Request $request){
+{   public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginname'=>'required',
+            'loginpassword'=>'required',
+        ]);
+        
+        if (auth()->attempt([
+            'name' => $incomingFields['loginname'],
+            'password' => $incomingFields['loginpassword']
+        ])) {
+            $request->session()->regenerate();
+        }
+        
+    return redirect('/');
+}
+    
+    public function logout (){
         auth()->logout();
         return redirect('/');
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,17 @@
 
             </form>
         </div>
+        <div style="border:3px solid black;">
+            <h2>Log In</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <input name="loginname" type="text" placeholder="name">
+                
+                <input name="loginpassword" type="password" placeholder="password">
+                <button>Log In</button>
+
+            </form>
+        </div>
     @endauth
 
 
