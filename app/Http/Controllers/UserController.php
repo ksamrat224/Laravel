6cd346891efe351ca79a5d0aa4b422782c71d431
ksamrat224 <ksamrat224@gmail.com>
2025-06-28 19:19:13 +0545

    rule for uniqueness

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register (Request $request){
         $incomingFields = $request->validate([
-            'name'=>'required|min:3|max:32',
-            'email'=>'required|email',
+            'name'=>'required|min:3|max:32'|Rule::unique('users','name'),
+            'email'=>'required|email'|Rule::unique('users','email'),
             'password'=>'required|min:8|max:32',
         ]);
         $incomingFields['password'] = bcrypt($incomingFields['password']);
